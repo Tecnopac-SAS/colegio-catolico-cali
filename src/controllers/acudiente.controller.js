c23574f0373fcf477b3eba5db23ac7ef4470a9d1
josRod128 <jose12836@gmail.com>
2023-02-03 15:18:55 -0700

    cafeteria

--- src/controllers/acudiente.controller.js
+++ src/controllers/acudiente.controller.js
@@ -46,5 +46,55 @@ acudienteCtrl.getBolsillo = async (req, res) => {
         res.send(error.message);
     }
 };
+acudienteCtrl.actualizarLonchera = async (req, res) => {
+    try {
+        let {idAcudiente,cant} = req.body;
+        if (idAcudiente === undefined || cant === undefined) {
+            res.status(400).json({ message: "Bad Request. Please fill all field." });
+        }
+        acudiente1 = await acudienteModel.findOne({
+            where: {
+                id: idAcudiente
+            }
+        })
+        acudiente = await acudienteModel.update({lonchera:cant+acudiente1.lonchera},{
+            where: {
+                id: idAcudiente
+            }
+        })
+        if (acudiente) {
+            res.json({
+                mensaje: 'Lonchera actualizada',
+                status: true
+            })
+        }else{
+            res.json({
+                mensaje: 'Tuvimos un error al ingresar tu saldo',
+                status: false
+            })
+        }
+
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
+acudienteCtrl.getLonchera = async (req, res) => {
+    try {
+        let {idAcudiente} = req.body;
+        if (idAcudiente === undefined) {
+            res.status(400).json({ message: "Bad Request. Please fill all field." });
+        }
+        acudiente = await acudienteModel.findOne({ where: { id: idAcudiente } })
+        
+        res.json({
+            resp: acudiente.lonchera
+        })
+
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 
 module.exports= acudienteCtrl
--- src/routes/acudiente.route.js
+++ src/routes/acudiente.route.js
@@ -4,6 +4,8 @@ const acudienteCtrl = require('../controllers/acudiente.controller');
 
 router.post('/addBolsillo',acudienteCtrl.actualizarBolsillo);
 router.post('/getBolsillo',acudienteCtrl.getBolsillo);
+router.post('/addLonchera',acudienteCtrl.actualizarLonchera);
+router.post('/getLonchera',acudienteCtrl.getLonchera);
 
 
 

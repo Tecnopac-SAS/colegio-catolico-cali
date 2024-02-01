2b916b8637ffe99202bb45b60399c751fd64075f
AndresBojaca <andresbojaca742@gmail.com>
2024-02-01 13:38:20 -0500

    fix aquire

--- src/db/databaseSq.js
+++ src/db/databaseSq.js
@@ -2,25 +2,28 @@ const Sequelize  = require('sequelize');
 const config = require("../../config");
 
 const sequelize = new Sequelize(
-   config.db.dbName,
-   config.db.user,
-   config.db.pass, 
-    {
-    host: config.db.host,
-    dialect: 'mysql',
-    dialectOptions: {
-      options: {
-          encrypt: true,
-      }
-    },
-    retry: {
-      match: [/Deadlock/i],
-      max: 3, // Maximum rety 3 times
-      backoffBase: 1000, // Initial backoff duration in ms. Default: 100,
-      backoffExponent: 1.5, // Exponent to increase backoff each try. Default: 1.1
-    },
-  });
-  
+  config.db.dbName,
+  config.db.user,
+  config.db.pass, 
+   {
+   host: config.db.host,
+   dialect: 'mysql',
+   dialectOptions: {
+     options: {
+         encrypt: true,
+     }
+   },
+   pool: {
+     acquire: 30000, // tiempo de espera en milisegundos (por ejemplo, 30 segundos)
+   },
+   retry: {
+     match: [/Deadlock/i],
+     max: 3, // Maximum rety 3 times
+     backoffBase: 1000, // Initial backoff duration in ms. Default: 100,
+     backoffExponent: 1.5, // Exponent to increase backoff each try. Default: 1.1
+   },
+ });
+
   try {
     sequelize.authenticate();
     console.log('Conexion a la base de datos exitosa: ' + config.db.dbName);

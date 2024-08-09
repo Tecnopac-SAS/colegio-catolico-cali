3ea26a0f4470f2a152a6b5553c29bbdd27e2ef9b
David Tecnopac <davidvalenzuela@tecnopac.com.co>
2024-08-08 22:47:45 -0500

    Update Backend

--- src/controllers/discount.controller.js
+++ src/controllers/discount.controller.js
@@ -1,14 +1,16 @@
 const {sequelize, Op} = require('sequelize');
 const discountModel = require('../models/discount.model')
+const {validationResult} = require("express-validator");
 const discountCtrl = {};
 
-discountCtrl.consultarDiscounts = async(req,res)=>{
+discountCtrl.consultarDiscounts = async (req, res) => {
+
     try {
         const result = await discountModel.findAll({where: {isActive: 1}});
         res.json({
             status: 200,
             mensaje: 'ok',
-            result:result
+            result: result
         })
     } catch (error) {
         res.status(500);
@@ -21,8 +23,8 @@ discountCtrl.consultarDiscounts = async(req,res)=>{
 
 discountCtrl.consultarDiscount = async (req, res) => {
     try {
-        const { name } = req.params;
-        const result = await discountModel.findAll({ where: { name:{[Op.like]:`${name}%`, isActive: 1}}});
+        const {name} = req.params;
+        const result = await discountModel.findAll({where: {name: {[Op.like]: `${name}%`, isActive: 1}}});
         res.json({
             mensaje: 'ok',
             result
@@ -35,8 +37,8 @@ discountCtrl.consultarDiscount = async (req, res) => {
 
 discountCtrl.consultarId = async (req, res) => {
     try {
-        const { id } = req.params;
-        const result = await discountModel.findOne({ where: { id: id, isActive: 1 } });
+        const {id} = req.params;
+        const result = await discountModel.findOne({where: {id: id, isActive: 1}});
         res.json({
             mensaje: 'ok',
             result
@@ -47,46 +49,57 @@ discountCtrl.consultarId = async (req, res) => {
     }
 };
 
-discountCtrl.crearDiscount = async(req,res)=>{
-    const {name,starDate,finalDate,percentage,useType,frequency,service,status}= req.body 
-
-     if(name==null){
-        res.json({
-            mensaje: 'Los campos deben estar diligenciados en su totalidad'
-        })
-    }
-    else {
-       
-        const data = await discountModel.create({name,starDate,finalDate,percentage,useType,frequency,service,status})
-        res.json({
-            mensaje: 'Curso creado',
-        })
+discountCtrl.crearDiscount = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
     }
 
+    const {name, starDate, finalDate, percentage, useType, frequency, service, status} = req.body
+
+    try {
+        await discountModel.create({name, starDate, finalDate, percentage, useType, frequency, service, status})
+        res.json({
+            mensaje: 'Descuento Creado',
+        })
+    } catch (error) {
+        req.status(500).send(error);
+    }
 }
 
 discountCtrl.actualizarDiscount = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
     try {
-        const { id } = req.params;
-        let {name,starDate,finalDate,percentage,useType,frequency,service,isActive,status} = req.body;
-        if (id === undefined || name === undefined) {
-            res.status(400).json({ message: "Bad Request. Please fill all field." });
-        }
-        await discountModel.update({name,starDate,finalDate,percentage,useType,frequency,service,isActive,status},{
+        const {id} = req.params;
+        let {name, starDate, finalDate, percentage, useType, frequency, service, isActive, status} = req.body;
+        await discountModel.update({
+            name,
+            starDate,
+            finalDate,
+            percentage,
+            useType,
+            frequency,
+            service,
+            isActive,
+            status
+        }, {
             where: {
                 id: id
             }
         })
-        const user = await discountModel.findOne({ where: { id: id } });
-         if(user === null){
+        const user = await discountModel.findOne({where: {id: id}});
+        if (user === null) {
             return res.json({
                 mensaje: 'descuento no encontrado',
             })
-        }
-        else {
+        } else {
             res.json({
                 mensaje: 'ok',
-                result:user
+                result: user
             })
         }
 
@@ -97,27 +110,29 @@ discountCtrl.actualizarDiscount = async (req, res) => {
 };
 
 discountCtrl.deshabilitar = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()})
+    }
+
     try {
-        const { id } = req.params;
-        const { status } = req.body;
-        if (status === null) {
-            res.status(400).json({ message: "Bad Request. Please fill all field." });
-        }
-        await discountModel.update({status},{
+        const {id} = req.params;
+        const {status} = req.body;
+
+        await discountModel.update({status}, {
             where: {
                 id: id
             }
         })
-        const user = await discountModel.findOne({ where: { id: id } });
-         if(user === null){
+        const user = await discountModel.findOne({where: {id: id}});
+        if (user === null) {
             return res.json({
                 mensaje: 'usuario no encontrado',
             })
-        }
-        else {
+        } else {
             res.json({
                 mensaje: 'ok',
-                result:user
+                result: user
             })
         }
 
@@ -129,26 +144,25 @@ discountCtrl.deshabilitar = async (req, res) => {
 
 discountCtrl.eliminar = async (req, res) => {
     try {
-        const { id } = req.params;
-        const { isActive } = req.body;
+        const {id} = req.params;
+        const {isActive} = req.body;
         if (isActive === null) {
-            res.status(400).json({ message: "Bad Request. Please fill all field." });
+            res.status(400).json({message: "Bad Request. Please fill all field."});
         }
-        await discountModel.update({isActive},{
+        await discountModel.update({isActive}, {
             where: {
                 id: id
             }
         })
-        const user = await discountModel.findOne({ where: { id: id } });
-         if(user === null){
+        const user = await discountModel.findOne({where: {id: id}});
+        if (user === null) {
             return res.json({
                 mensaje: 'usuario no encontrado',
             })
-        }
-        else {
+        } else {
             res.json({
                 mensaje: 'ok',
-                result:user
+                result: user
             })
         }
 
@@ -158,4 +172,4 @@ discountCtrl.eliminar = async (req, res) => {
     }
 };
 
-module.exports= discountCtrl
+module.exports = discountCtrl
--- src/routes/discount.route.js
+++ src/routes/discount.route.js
@@ -1,16 +1,38 @@
 const {Router} = require('express');
 const router = Router();
 const DiscountCtrl = require('../controllers/discount.controller');
+const {check} = require("express-validator");
+
+const frequencyType = ['Unico', 'frecuente'];
+const serviceType = ['Pension', 'Matricula']
 
 router.get('/listarDiscounts',DiscountCtrl.consultarDiscounts);
 router.get('/listarDiscount/:name',DiscountCtrl.consultarDiscount);
 router.get('/listarDiscountId/:id',DiscountCtrl.consultarId);
 router.post('/crearDiscount',[
-    //name,starDate,finalDate,percentage,useType,frequency,service,status
+    check("name").notEmpty().withMessage("Ingrese el campo name"),
+    check("starDate").notEmpty().withMessage("Ingresa el campo starDate"),
+    check("finalDate").notEmpty().withMessage("Ingresa el campo finalDate"),
+    check("percentage").notEmpty().withMessage("Ingresa el campo percentage"),
+    check("useType").optional(),
+    check("frequency").notEmpty().withMessage("Ingresa el campo frequency").bail().isIn(frequencyType).withMessage("Ingresa frequency correctamente"),
+    check("service").notEmpty().withMessage("Ingresa el campo service").bail().isIn(serviceType).withMessage("El tipo de servicio no es valido"),
+    check("status").notEmpty().withMessage("Ingresa el campo status").bail().isBoolean().withMessage("Este campo tiene que ser booleano")
 ],DiscountCtrl.crearDiscount);
-router.put('/actualizarDiscount/:id',DiscountCtrl.actualizarDiscount)
-router.put('/deshabilitar/:id' ,DiscountCtrl.deshabilitar)
-router.put('/eliminar/:id' ,DiscountCtrl.eliminar)
+router.put('/actualizarDiscount/:id',
+    check("name").notEmpty().withMessage("Ingrese el campo name"),
+    check("starDate").notEmpty().withMessage("Ingresa el campo starDate"),
+    check("finalDate").notEmpty().withMessage("Ingresa el campo finalDate"),
+    check("percentage").notEmpty().withMessage("Ingresa el campo percentage"),
+    check("useType").notEmpty().withMessage("Ingresa el campo useType"),
+    check("frequency").notEmpty().withMessage("Ingresa el campo frequency").bail().isIn(frequencyType).withMessage("Ingresa frequency correctamente"),
+    check("service").notEmpty().withMessage("Ingresa el campo service").bail().isIn(serviceType).withMessage("El tipo de servicio no es valido"),
+    check("status").notEmpty().withMessage("Ingresa el campo status").bail().isBoolean().withMessage("Este campo tiene que ser booleano")
+    ,DiscountCtrl.actualizarDiscount)
+router.put('/deshabilitar/:id' ,[
+    check("status").notEmpty().withMessage("Ingresa el campo status").bail().isBoolean().withMessage("El campo status es tipo boolean")
+],DiscountCtrl.deshabilitar)
+router.delete('/eliminar/:id' ,DiscountCtrl.eliminar)
 
 
 
